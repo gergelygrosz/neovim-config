4d726c93544c5b5978abba6c0883e259e3c1648f
gergelygrosz <groszgergely09@gmail.com>
2025-07-23 17:44:08 +0200

    New.

--- lua/config/grosz.lua
+++ lua/config/grosz.lua
@@ -1,7 +1,10 @@
-require("config.grosz.remap")
-
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -4,7 +4,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
 
@@ -34,7 +33,6 @@ require("lazy").setup({
     { import = "plugins" },
 
   },
-
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
